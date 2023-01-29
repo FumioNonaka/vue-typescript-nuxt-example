2db85cc7f26465ffacc1428b7481fcc84d89ad53
Fumio Nonaka <fumio@mx10.ttcn.ne.jp>
2023-01-29 10:27:24 +0900

    Vue 3 + TypeScript: Nuxt 3 example 01

---
+++ components/Advertisement.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+type AddType = {
+	id: number;
+	title: string;
+	url: string;
+};
+const _ads: AddType[] = [{
+  id: 1,
+  title: "エンジニア募集中",
+  url: "https://wwwrecruit.mamezou.com/"
+}, {
+  id: 2,
+  title: "オンラインセミナー開催のお知らせ",
+  url: "https://mamezou.connpass.com/"
+}];
+const ads = ref(_ads);
+</script>
+
+<template>
+  <hr />
+  <p style="margin: 0.2em 0">広告</p>
+  <ul style="list-style-type:none;padding-left:0;">
+    <li v-for="ad in ads" :key="ad.id"><a :href="ad.url">{{ ad.title }}</a></li>
+  </ul>
+</template>
