62b1e427d9f5eaec9a88deb19e9242829c995a5b
Fumio Nonaka <fumio@mx10.ttcn.ne.jp>
2023-01-29 15:12:47 +0900

    Vue 3 + TypeScript: Nuxt 3 example 02

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,22 @@
+<script lang="ts">
+export {};
+</script>
+
 <script setup lang="ts">
-const { fetchArticles, articles } = useArticles();
-fetchArticles();
+const { data: articles, refresh } = await useFetch('/api/blogs');
 </script>
 
 <template>
   <div>
-    <div class="container">
-      <p>新着記事！！</p>
-      <ul>
-        <li v-for="article in articles" :key="article.id">
-          <NuxtLink :to="{path: '/details', query: { id:article.id }}"> 
-            {{article.title}}
-          </NuxtLink>
-        </li>
-      </ul>
-    </div>
+    <p>新着記事！！</p>
+    <ul>
+      <li v-for="article in articles" :key="article.id">
+        <NuxtLink :to="{path: '/details', query: { id:article.id }}">
+          {{article.title}}
+        </NuxtLink>
+      </li>
+    </ul>
+    <button @click="() => refresh()">最新情報取得</button>
     <Advertisement />
   </div>
 </template>
-
-<style scoped>
-header {
-  border-bottom-style: solid;
-  padding: 1rem;
-}
-footer {
-  margin-top: 2rem;
-  background-color: #8080ee;
-  padding: 1rem;
-}
-.container {
-  margin: 2rem;
-}
-</style>
